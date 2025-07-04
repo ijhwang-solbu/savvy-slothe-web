5ea45514aea4f6186e3efb28f838eb673a427bca
ijhwang <ij.hwang666@gmail.com>
2025-07-05 01:14:18 +0900

    회원가입/로그인 테스트 코드 추가2

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 'use client';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://igdpvanunwvpzkukwymr.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI...';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
