a1cc6ff94c718f775ecf4021bf7c7f784d4a60ee
ijhwang <ij.hwang666@gmail.com>
2025-07-31 23:20:00 +0900

    게이지바 개선, 팝업 모달 생성

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,8 @@ import TaskExecutionPanel from './components/TaskExecutionPanel';
 import Link from 'next/link';
 import AutoLogoutWrapper from './components/AutoLogoutWrapper';
 import styles from './text.module.css';
+import modal from './components/modal/modal';
+import Modal from './components/modal/modal';
 
 export default function Home() {
   /* ==========================================
@@ -84,7 +86,33 @@ export default function Home() {
   ================================= */
   const fetchTasks = async () => {
     const { data, error } = await supabase.from('v_user_tasks').select('*').order('created_at', { ascending: false });
-    setTasks(data || []);
+
+    if (error) {
+      console.error('태스크 조회 오류:', error);
+      return;
+    }
+
+    if (data) {
+      const today = new Date();
+
+      const updatedTasks = data.map((task) => {
+        // 경과일 계산
+        const daysPassed = Math.floor((today.setHours(0, 0, 0, 0) - new Date(task.start_date).setHours(0, 0, 0, 0)) / (1000 * 60 * 60 * 24)) + 1;
+
+        // 예상 실행 횟수
+        const expectedCount = (daysPassed / task.interval_days) * task.target_count;
+
+        // 성공률 계산
+        const successRatio = expectedCount > 0 ? task.execution_count / expectedCount : 0;
+
+        return {
+          ...task,
+          success_ratio: successRatio,
+        };
+      });
+
+      setTasks(updatedTasks);
+    }
   };
 
   useEffect(() => {
@@ -123,7 +151,7 @@ export default function Home() {
 ========================================== */
   return (
     <AutoLogoutWrapper>
-      <main style={{ padding: '2rem' }}>
+      <main style={{ padding: '1rem' }}>
         <h1 style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           Savvy Sloth
           <button
@@ -158,30 +186,29 @@ export default function Home() {
           <strong>새 결심 등록하기</strong>
         </button>
 
-        {showModal && (
-          <div style={{ border: '1px solid #333', padding: '1rem', marginTop: '0.5rem' }}>
-            <h3 className={styles.sectionTitle}>새로운 결심</h3>
-            <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', gap: '0.5rem', margin: '1rem 0rem' }}>
-              <input style={{ ...inputStyle, width: '8rem' }} placeholder='무엇을' value={title} onChange={(e) => setTitle(e.target.value)} />
-              <span>을 </span>
+        {/* 등록 모달 */}
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <h3 className={styles.sectionTitle}>새로운 결심</h3>
+          <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', gap: '0.5rem', margin: '1rem 0rem' }}>
+            <input style={{ ...inputStyle, width: '8rem' }} placeholder='20분 공원 뛰기' value={title} onChange={(e) => setTitle(e.target.value)} />
+            <span> ☆ </span>
 
-              <input style={{ ...inputStyle, width: '4rem' }} placeholder='5' value={intervalDays} onChange={(e) => setIntervalDays(e.target.value)} type='number' min='1' />
-              <span>일에</span>
+            <input style={{ ...inputStyle, width: '4rem' }} placeholder='5' value={intervalDays} onChange={(e) => setIntervalDays(e.target.value)} type='number' min='1' />
+            <span>일에</span>
 
-              <input style={{ ...inputStyle, width: '4rem' }} placeholder='2' value={targetCount} onChange={(e) => setTargetCount(e.target.value)} type='number' min='1' />
-              <span>번씩만이라도 해보까?</span>
-            </div>
-
-            <div>
-              <button onClick={insertTask} style={{ ...btn, background: '#f4e3ffff' }}>
-                등록
-              </button>
-              <button onClick={() => setShowModal(false)} style={btn}>
-                취소
-              </button>
-            </div>
+            <input style={{ ...inputStyle, width: '4rem' }} placeholder='2' value={targetCount} onChange={(e) => setTargetCount(e.target.value)} type='number' min='1' />
+            <span>번 씩 만이라도 해보까?</span>
           </div>
-        )}
+
+          <div>
+            <button onClick={insertTask} style={{ ...btn, background: '#f4e3ffff' }}>
+              등록
+            </button>
+            <button onClick={() => setShowModal(false)} style={btn}>
+              취소
+            </button>
+          </div>
+        </Modal>
 
         <hr />
 
@@ -191,6 +218,14 @@ export default function Home() {
           const today = new Date(); // 브라우저가 한국 시간대라면 이미 KST임
           const startDate = new Date(task.start_date);
           const daysPassed = Math.floor((new Date(today.getFullYear(), today.getMonth(), today.getDate()) - new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate())) / (1000 * 60 * 60 * 24)) + 1;
+          const rawPercent = task.success_ratio * 100; //게이지 계산
+          const cappedPercent = Math.min(rawPercent, 200); //게이지 계산
+          let markerPos;
+          if (cappedPercent <= 100) {
+            markerPos = (cappedPercent / 100) * 65;
+          } else {
+            markerPos = 65 + ((cappedPercent - 100) / 100) * 30;
+          }
           return (
             <div
               key={task.id}
@@ -215,11 +250,11 @@ export default function Home() {
                   rowGap: '0.5rem',
                 }}>
                 <div style={{ marginRight: '10px' }}>
-                  <p>
+                  {/* <p>
                     <strong>{task.start_date}</strong>에 시작해서
-                  </p>
+                  </p> */}
                   <p>
-                    <strong>{daysPassed}일</strong> 동안 <strong>{task.execution_count}번</strong> 진행했네요.
+                    <strong>총 {daysPassed}일</strong> 동안 <strong>{task.execution_count}번</strong> 진행했네요.
                   </p>
                 </div>
                 <Link
@@ -246,30 +281,58 @@ export default function Home() {
               ) : (
                 // 실행한 적 있는 경우: 게이지 + 메시지
                 <div style={{ marginTop: '0.5rem' }}>
-                  <p style={{ fontWeight: 'bold', marginBottom: '0.25rem' }}>현재 페이스: {(task.success_ratio * 100).toFixed(1)}%</p>
+                  {/* <p style={{ fontWeight: 'bold', marginBottom: '0.25rem' }}>현재 페이스: {(task.success_ratio * 100).toFixed(1)}%</p> */}
+                  <p style={{ fontWeight: 'bold', marginBottom: '0.25rem' }}>
+                    페이스: {(task.success_ratio * task.target_count).toFixed(1)}회 / {task.interval_days}일
+                  </p>
 
+                  {/* 게이지 바 */}
                   <div
                     style={{
+                      position: 'relative',
                       width: '100%',
-                      height: '10px',
-                      backgroundColor: '#eee',
-                      borderRadius: '5px',
-                      overflow: 'hidden',
+                      height: '14px',
+                      background: 'linear-gradient(to right, #eb5050ff, #22c55e 50%, #3b82f6)',
+                      borderRadius: '7px',
                     }}>
+                    {/* 100% 기준선 */}
                     <div
                       style={{
-                        width: `${(task.success_ratio * 100).toFixed(1)}%`,
+                        position: 'absolute',
+                        top: 0,
+                        left: `65%`, // 100% 위치를 게이지의 75% 지점에 매핑
+                        width: '2px',
                         height: '100%',
-                        backgroundColor: task.success_ratio >= 1.1 ? '#22c55e' : task.success_ratio >= 0.8 ? '#60a5fa' : task.success_ratio >= 0.6 ? '#facc15' : '#ef4444',
-                        transition: 'width 0.5s ease',
+                        backgroundColor: '#fff',
+                        opacity: 0.8,
+                      }}
+                    />
+
+                    {/* 마커 */}
+                    <div
+                      style={{
+                        position: 'absolute',
+                        top: '2px',
+                        left: `calc(${markerPos}% - 4px)`,
+                        width: '10px',
+                        height: '10px',
+                        background: '#f52828ff',
+                        border: '2px solid #ffffffff',
+                        transform: 'rotate(45deg)',
+                        borderRadius: '2px',
+                        boxShadow: '0 0 6px 3px rgba(179, 0, 149, 0.8)',
+                        transition: 'left 0.5s ease',
                       }}
                     />
                   </div>
 
+                  {/* --------------- */}
+
                   {/* 상태 메시지 */}
-                  {task.success_ratio >= 1.1 && <p style={{ color: '#22c55e', marginTop: '4px' }}>완벽 페이스 유지 중! 날아가요!</p>}
-                  {task.success_ratio >= 0.9 && task.success_ratio < 1.1 && <p style={{ color: '#60a5fa', marginTop: '4px' }}>잘 하고 있어요. 이대로만 해도 충분해요.</p>}
-                  {task.success_ratio >= 0.7 && task.success_ratio < 0.9 && <p style={{ color: '#facc15', marginTop: '4px' }}>조금 느려졌어요. 다시 가볼까요?</p>}
+                  {task.success_ratio >= 1.3 && <p style={{ color: '#22c55e', marginTop: '4px' }}>매우 빠릅니다!!!! 날아가요!</p>}
+                  {task.success_ratio >= 1.0 && task.success_ratio < 1.3 && <p style={{ color: '#60a5fa', marginTop: '4px' }}>완벽 페이스 유지 중!! 이대로 쭉 가자요!</p>}
+                  {task.success_ratio >= 0.85 && task.success_ratio < 1.0 && <p style={{ color: '#60a5fa', marginTop: '4px' }}>잘 하고 있어요. 이대로만 해도 충분해요.</p>}
+                  {task.success_ratio >= 0.7 && task.success_ratio < 0.85 && <p style={{ color: '#facc15', marginTop: '4px' }}>조금 느려졌어요. 다시 가볼까요?</p>}
                   {task.success_ratio < 0.7 && <p style={{ color: '#ef4444', marginTop: '4px' }}>힘이 빠졌어요. 따라가려면 좀 더 힘을 내야 해요.</p>}
                 </div>
               )}
